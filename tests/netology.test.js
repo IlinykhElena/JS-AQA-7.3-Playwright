6322cc86cf35b45688d0de5aca22821e6ddd3ba9
ElenaIlinykh <bengaliki22@gmail.com>
2023-03-26 23:10:50 +0700

    made beforeEach and afterEach

--- tests/netology.test.js
+++ tests/netology.test.js
@@ -1,89 +1,82 @@
 const { chromium } = require("playwright");
-//const { test, expect } = require("@playwright/test");
 
 import { email } from "./user";
 import { password } from "./user";
 
-(async () => {
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 5000,
-    devtools: true,
-  });
+const browser = await chromium.launch({
+  headless: false,
+  slowMo: 5000,
+  devtools: true,
+});
 
-  const page = (await browser).newPage();
+const page = (await browser).newPage();
 
-  test("sucsessful login", async ({ page }) => {
-    await page.goto("https://netology.ru/");
-    await expect(page).toHaveURL("https://netology.ru/");
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://netology.ru/");
+});
 
-    await page.getByRole("link", { name: "Войти" }).click();
-    await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+test.afterAll(async () => {
+  await browser.close();
+});
 
-    await page.getByPlaceholder("Email").click();
-    await page.getByPlaceholder("Email").fill("email");
-    await page.getByPlaceholder("Пароль").click();
-    await page.getByPlaceholder("Пароль").fill("password");
-    await page.getByTestId("login-submit-btn").click();
-    await expect(page).toHaveURL("https://netology.ru/profile");
-    await browser.close();
-  });
+test("sucsessful login", async ({ page }) => {
+  await expect(page).toHaveURL("https://netology.ru/");
 
-  test("login with false email", async ({ page }) => {
-    var faker = require("faker");
-    await page.goto("https://netology.ru/");
-    await expect(page).toHaveURL("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
 
-    await page.getByRole("link", { name: "Войти" }).click();
-    await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("email");
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill("password");
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page).toHaveURL("https://netology.ru/profile");
+});
 
-    await page.getByPlaceholder("Email").click();
-    await page.getByPlaceholder("Email").fill(faker.internet.email());
-    await page.getByPlaceholder("Пароль").click();
-    await page.getByPlaceholder("Пароль").fill("password");
-    await page.getByTestId("login-submit-btn").click();
-    await expect(page).toHaveURL("https://netology.ru/profile");
-    await browser.close();
-  });
+test("login with false email", async ({ page }) => {
+  var faker = require("faker");
+  await expect(page).toHaveURL("https://netology.ru/");
 
-  test("login with false password", async ({ page }) => {
-    var faker = require("faker");
-    await page.goto("https://netology.ru/");
-    await expect(page).toHaveURL("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
 
-    await page.getByRole("link", { name: "Войти" }).click();
-    await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(faker.internet.email());
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill("password");
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page).toHaveURL("https://netology.ru/profile");
+});
 
-    await page.getByPlaceholder("Email").click();
-    await page.getByPlaceholder("Email").fill("email");
-    await page.getByPlaceholder("Пароль").click();
-    await page.getByPlaceholder("Пароль").fill(faker.random());
-    await page.getByTestId("login-submit-btn").click();
-    await expect(page).toHaveURL("https://netology.ru/profile");
-    await browser.close();
-  });
+test("login with false password", async ({ page }) => {
+  var faker = require("faker");
+  await expect(page).toHaveURL("https://netology.ru/");
 
-  test("login with empty email", async ({ page }) => {
-    // Go to https://netology.ru/free/management#/
-    await page.goto("https://netology.ru/");
-    await expect(page).toHaveURL("https://netology.ru/");
-    await page.getByRole("link", { name: "Войти" }).click();
-    await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
-    await page.getByPlaceholder("Пароль").click();
-    await page.getByPlaceholder("Пароль").fill("password");
-    await expect().toHaveText("Обязательное поле");
-    await browser.close();
-  });
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
 
-  test("login with empty password", async ({ page }) => {
-    // Go to https://netology.ru/free/management#/
-    await page.goto("https://netology.ru/");
-    await expect(page).toHaveURL("https://netology.ru/");
-    await page.getByRole("link", { name: "Войти" }).click();
-    await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
-    await page.getByPlaceholder("Email").click();
-    await page.getByPlaceholder("Email").fill("email");
-    await expect().toHaveText("Обязательное поле");
-    await browser.close();
-  });
-})();
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("email");
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill(faker.random());
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page).toHaveURL("https://netology.ru/profile");
+});
+
+test("login with empty email", async ({ page }) => {
+  await expect(page).toHaveURL("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill("password");
+  await expect().toHaveText("Обязательное поле");
+});
+
+test("login with empty password", async ({ page }) => {
+  await expect(page).toHaveURL("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("email");
+  await expect().toHaveText("Обязательное поле");
+});
